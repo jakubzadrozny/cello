fd5ff53eac546871c8702e8c6c13d0786a43cd44
Charles Peyrat <charlesp@crans.org>
2020-10-20 13:19:44 +0200

    Implemented crossover method along with helper function currentlyAssignedGroup

--- src/main/java/org/cellocad/MIT/dnacompiler/BuildCircuitsGenetic.java
+++ src/main/java/org/cellocad/MIT/dnacompiler/BuildCircuitsGenetic.java
@@ -41,6 +41,77 @@ public class BuildCircuitsGenetic extends BuildCircuits {
         super(options, gate_library, roadblock);
     }
 
+    private boolean currentlyAssignedGroup(LogicCircuit lc, String group_name) {
+
+        for (Gate g: lc.get_logic_gates()) {
+            if (g.group.equals(group_name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private LogicCircuit crossover(LogicCircuit lc1, LogicCircuit lc2) {
+
+        Random generator = new Random();
+
+        LogicCircuit child = new LogicCircuit(lc1);
+
+        int cutoff_point =  /* GENERATE RANDOM CUTOFF POINT */;
+
+        for (int i = cutoff_point; i < lc2.get_logic_gates().size(); i++) {
+
+            Gate g_child = child.get_logic_gates().get(i);
+            Gate g_parent = lc2.get_logic_gates().get(i);
+            g_child.name = g_parent.name;
+            g_child.groupe = g_parent.group;
+
+        }
+
+        for (int i = 0; i < child.get_logic_gates().size(); i++) {
+
+            Gate g = child.get_logic_gates().get(i);
+
+            String g_name = new String(g.name);
+            String g_group = new String(g.group);
+            g.name = "null";
+            g.group = "null";
+
+            if (currentlyAssignedGroup(child, g_group)) {
+
+                LinkedHashMap<String, ArrayList<Gate>> groups_of_type = get_gate_library().get_GATES_BY_GROUP().get(g.type);
+
+                ArrayList<String> group_names = new ArrayList<String>(groups_of_type.keySet());
+
+                Collections.shuffle(group_names);
+
+                for (String group_name : group_names) {
+
+                    if (!currentlyAssignedGroup(lc, group_name)) {
+
+                        ArrayList<Gate> gates_of_group = new ArrayList<Gate>(groups_of_type.get(group_name));
+                        
+                        Collections.shuffle(gates_of_group);
+
+                        g.name = gates_of_group.get(0).name;
+                        g.group = group_name;
+
+                    }
+                }
+            }
+
+            else {
+
+                g.name = g_name;
+                g.group = g_group;
+
+            }
+        }
+
+        return child;
+
+    }
 
     /**
      *  returns void, but sets the list of assigned circuits, which can be accessed through 'get_logic_circuits()'
